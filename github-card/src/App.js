c85ec2d11948cabc15c193a10f3c879d10eb596c
Steven Sussman <sjsussman@gmail.com>
2020-09-29 18:15:43 -0400

    rendered user and followers

--- github-card/src/App.js
+++ github-card/src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    users: [],
+    followers: [],
+    url: "",
+  };
+
+  componentDidMount() {
+    this.fetchUser();
+    this.fetchFollowers();
+  }
+
+  fetchUser = () => {
+    fetch("https://api.github.com/users/agyin3")
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          users: res,
+        });
+      });
+  };
+
+  // fetchFollowers = () => {
+  //   fetch("https://api.github.com/users/sjsussman/followers")
+  //     .then((res) => res.json())
+  //     .then((res) => {
+  //       this.setState({
+  //         followers: res,
+  //       });
+  //       console.log(res);
+  //     });
+  // };
+
+  fetchFollowerUrl = (followers) => {
+    fetch(`https://api.github.com/users/${followers}`)
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          url: res.url,
+        });
+        console.log(res);
+      });
+  };
+
+  handleUrl = () => {
+    this.fetchFollowerUrl(this.state.url);
+  };
+
+  // handleUrl = (e) => {
+  //   this.setState({
+  //     followers: e.target.value
+  //   });
+  // };
+
+  render() {
+    return (
+      <div>
+        <div className="userContainer">
+          <img src={this.state.users.avatar_url} width="200px" alt="user img" />
+          <div>{this.state.users.name}</div>
+          <div>{this.state.users.location}</div>
+          <div>{this.state.users.bio}</div>
+        </div>
+        <div className="followerContainer">
+          {this.state.followers.map((follower) => (
+            <div key={follower.id}>
+              <img
+                src={follower.avatar_url}
+                width="200px"
+                onClick={this.handleUrl}
+              />{" "}
+              <br />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
