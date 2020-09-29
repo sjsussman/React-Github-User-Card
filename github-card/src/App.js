1e46204b747de022d3ace250806804ea8d2ab554
Steven Sussman <sjsussman@gmail.com>
2020-09-29 18:57:57 -0400

    added follower profiles

--- github-card/src/App.js
+++ github-card/src/App.js
@@ -34,27 +34,6 @@ class App extends React.Component {
       });
   };
 
-  fetchFollowerUrl = (followers) => {
-    fetch(`https://api.github.com/users/${followers}`)
-      .then((res) => res.json())
-      .then((res) => {
-        this.setState({
-          url: res.url,
-        });
-        console.log(res);
-      });
-  };
-
-  handleUrl = () => {
-    this.fetchFollowerUrl(this.state.url);
-  };
-
-  // handleUrl = (e) => {
-  //   this.setState({
-  //     followers: e.target.value
-  //   });
-  // };
-
   render() {
     return (
       <div>
@@ -62,16 +41,15 @@ class App extends React.Component {
           <img src={this.state.users.avatar_url} width="200px" alt="user img" />
           <div>{this.state.users.name}</div>
           <div>{this.state.users.location}</div>
-          <div>{this.state.users.bio}</div>
+          <div>{this.state.users.bio}</div> <br />
+          <div>Followers:</div> <br />
         </div>
         <div className="followerContainer">
           {this.state.followers.map((follower) => (
             <div key={follower.id}>
-              <img
-                src={follower.avatar_url}
-                width="200px"
-                onClick={this.handleUrl}
-              />{" "}
+              <a href={follower.html_url}>
+                <img src={follower.avatar_url} width="200px" />{" "}
+              </a>
               <br />
             </div>
           ))}
